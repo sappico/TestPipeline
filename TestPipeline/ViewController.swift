3961574739b12b0e01b3abe0a291cd559ab08c73
Kris <kris.weytjens@ing.be>
2018-10-16 14:04:39 +0200

    Initial commit

---
+++ TestPipeline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPipeline
+//
+//  Created by Kris Weytjens on 16/10/2018.
+//  Copyright © 2018 Sappico V.O.F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
